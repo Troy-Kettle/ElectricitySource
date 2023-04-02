0e4e0b6df255d8ca9ddfb5866402966136671031
Troy-Kettle <89986560+Troy-Kettle@users.noreply.github.com>
2023-04-02 11:42:45 +0100

    Update Tabs.js

--- Tabs.js
+++ Tabs.js
@@ -4,29 +4,31 @@ import CsvTable from "./csv";
 import BarChart from "./BarChart";
 
 const data = `label,value
-    10:00 ,42.20
-    12:00 ,43.01
-    14:00 ,43.01
-    16:00 ,43.21
-    18:00 ,42.08
-  `;
+  10:00 ,42.20
+  12:00 ,43.01
+  14:00 ,43.01
+  16:00 ,43.21
+  18:00 ,42.08
+`;
 
-export default () => (
-  <Tabs>
-    <TabList>
-      <Tab>Bar Chart Data</Tab>
-      <Tab>Live Data Table</Tab>
-    </TabList>
-    <TabPanel>
-      <div className="Bars">
-        <h4>Today's Price</h4>
-        <BarChart data={data} />
-      </div>
-    </TabPanel>
-    <TabPanel>
-      <div className="CSVTable">
-        <CsvTable />
-      </div>
-    </TabPanel>
-  </Tabs>
-);
+export default function App() {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Bar Chart Data</Tab>
+        <Tab>Live Data Table</Tab>
+      </TabList>
+      <TabPanel>
+        <div className="Bars">
+          <h4>Today's Price</h4>
+          <BarChart data={data} />
+        </div>
+      </TabPanel>
+      <TabPanel>
+        <div className="CSVTable">
+          <CsvTable />
+        </div>
+      </TabPanel>
+    </Tabs>
+  );
+}
