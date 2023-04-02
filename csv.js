fdfb69ce59deac68f4fb19b98aceac863a9bc7fa
Troy-Kettle <89986560+Troy-Kettle@users.noreply.github.com>
2023-04-02 13:34:10 +0100

    Update csv.js

--- csv.js
+++ csv.js
@@ -1,38 +1,41 @@
-import React, { useState } from 'react';
-import Papa from 'papaparse';
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+import csvFile from "./react.csv";
 
 function CsvTable() {
   const [data, setData] = useState([]);
 
-  const handleCsvUpload = (event) => {
-    const file = event.target.files[0];
-    Papa.parse(file, {
+  const handleCsvUpload = () => {
+    Papa.parse(csvFile, {
       header: true,
+      download: true,
       complete: (result) => {
         setData(result.data);
       },
     });
   };
 
+  // Call the handleCsvUpload function when the component mounts
+  useEffect(() => {
+    handleCsvUpload();
+  }, []);
+
   return (
     <div>
-      <input type="file" onChange={handleCsvUpload} />
-      <table className='Table'>
+      <table className="Table">
         <thead>
-            <br></br>
           <tr>
             <th>Yesterday</th>
             <th>Today</th>
             <th>Last Year</th>
           </tr>
         </thead>
-        <br></br>
         <tbody>
           {data.map((row, index) => (
             <tr key={index}>
-              <td>{row['Column 1']}</td>
-              <td>{row['Column 2']}</td>
-              <td>{row['Column 3']}</td>
+              <td>{row["Column 1"]}</td>
+              <td>{row["Column 2"]}</td>
+              <td>{row["Column 3"]}</td>
             </tr>
           ))}
         </tbody>
